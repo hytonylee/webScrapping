a10ecf9bcdf37c1c90f30b39847ca82b4c66dd4b
Tony Lee <hy.tony.lee@gmail.com>
2020-02-08 15:22:58 -0800

    Read file by line

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const readline = require('readline');
+const stream = require('stream');
+
+const inStream = fs.createReadStream('./name_list.txt');
+const outStream = new stream();
+const rl = readline.createInterface(inStream, outStream);
+
+// try {
+// 	const data = fs.readFileSync('name_list.txt', 'utf8');
+// 	console.log('Here is data: ' + data);
+// } catch (err) {
+// 	console.error(err);
+// }
+
+rl.on('line', function(line) {
+	// process line here
+	console.log(line + '2');
+});
 
 const getPic = async function() {
 	const browser = await puppeteer.launch({ headless: true });
