ac9b971eca1273d3eb2be008a8e6913208140739
Tony Lee <hy.tony.lee@gmail.com>
2020-02-08 14:17:39 -0800

    Screenshot capture test success

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const puppeteer = require('puppeteer');
 
 const getPic = async function() {
-  const
-}
+	const browser = await puppeteer.launch({ headless: true });
+	const page = await browser.newPage();
+	await page.goto('https://google.ca');
+	await page.setViewport({ width: 1000, height: 500 });
+	await page.screenshot({ path: 'google.png' });
+
+	await browser.close();
+};
+
+getPic();
